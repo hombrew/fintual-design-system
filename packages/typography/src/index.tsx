8ba99f3e19327dfa4789b9a7d856537754db2dd4
Manuel Maldonado <mmaldonadortiz92@gmail.com>
2026-01-10 00:07:29 +0000

    feat(workspace:typography): created typography lib

---
+++ packages/typography/src/index.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export interface ThingProps {
+  children?: ReactNode;
+}
+
+export const Thing = ({ children }: ThingProps) => {
+  return <div>{children ?? "the snozzberries taste like snozzberries"}</div>;
+};
